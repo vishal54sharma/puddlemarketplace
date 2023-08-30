87774aeff9bea4cdd45337b7ca9f5f92ed05e646
vishal54sharma <vishal54sharma@gmail.com>
2023-08-30 17:48:52 +0530

    first commit

---
+++ item/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Item
+# Create your views here.
+
+def detail(request, pk):
+    item = get_object_or_404(Item,id=pk)
+    return render(request,'item/detail.html',{'item':item})
